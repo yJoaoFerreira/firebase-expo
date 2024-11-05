5f9412179b23010cf7291c401a13e286b48c29b2
João Ferreira <yFerreiraJoao@gmail.com>
2024-11-05 14:34:43 -0300

    Update firebaseConfig.js

--- exemplo-app/firebaseConfig.js
+++ exemplo-app/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWrYOAkogpzlY9YQAztePer57ZJXuPfH0",
-  authDomain: "tutorial-14929.firebaseapp.com",
-  projectId: "tutorial-14929",
-  storageBucket: "tutorial-14929.appspot.com",
-  messagingSenderId: "424236976540",
-  appId: "1:424236976540:web:9e2c1fecd89a474ed0a059",
-  measurementId: "G-0R3K67R2J9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
